2534e578778724dbe7c067bdf21cfda75d79550c
Syed Muaz <smuaz@yahoo.com>
2015-06-11 04:07:38 +0800

    added sign in, register, homepage, search and category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+comedies = Category.create(name: 'TV Comedy')
+dramas = Category.create(name: 'TV Drama')
+reality = Category.create(name: 'Reality TV')
+
+Video.create(title: 'Futurama', description: 'Awesome TV show', category: comedies, small_cover_url: 'futurama.jpg', large_cover_url: 'futurama_large.jpg')
+Video.create(title: 'South Park', description: 'Great TV show', category: comedies, small_cover_url: 'south_park.jpg', large_cover_url: 'south_park_large.jpg')
+Video.create(title: 'Family Guy', description: 'Peter Griffin and talking dog TV show', category: comedies, small_cover_url: 'family_guy.jpg', large_cover_url: 'family_guy_large.jpg')
+Video.create(title: 'Futurama', description: 'Awesome TV show', category: comedies, small_cover_url: 'futurama.jpg', large_cover_url: 'futurama_large.jpg')
+Video.create(title: 'South Park', description: 'Great TV show', category: comedies, small_cover_url: 'south_park.jpg', large_cover_url: 'south_park_large.jpg')
+Video.create(title: 'South Park', description: 'Great TV show', category: comedies, small_cover_url: 'south_park.jpg', large_cover_url: 'south_park_large.jpg')
+Video.create(title: 'Family Guy', description: 'Peter Griffin and talking dog TV show', category: comedies, small_cover_url: 'family_guy.jpg', large_cover_url: 'family_guy_large.jpg')
+
+Video.create(title: 'Monk', description: 'Funny TV show', category: dramas, small_cover_url: 'monk.jpg', large_cover_url: 'monk_large.jpg')
+Video.create(title: 'Monk', description: 'Funny TV show', category: dramas, small_cover_url: 'monk.jpg', large_cover_url: 'monk_large.jpg')
+
+
+
